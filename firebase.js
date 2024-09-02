33908d6fdd67a4272de892ebd3c8d3425870f6ea
Prainex <51273142+Prainex@users.noreply.github.com>
2024-09-02 17:07:29 -0400

    fixed up firebase.js and api/chat: route.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {getFirestore} from "firebase/firestore";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,12 +13,9 @@ const firebaseConfig = {
   storageBucket: "habittracker-aa6c8.appspot.com",
   messagingSenderId: "867826382009",
   appId: "1:867826382009:web:12264c24c0f9e485101768",
-  measurementId: "G-C9XKY8E4Z3"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-const analytics = getAnalytics(app);
-
 export {db};
